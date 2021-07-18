07167376ce9609e55f607153555dcc1ba25b1f18
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-18 15:36:32 +0300

    Libraries upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <GoogleAuthorization.h>
+#include <FacebookAuthorization.h>
+#include <Authorization.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello world!" << endl;
+	return 0;
+}
