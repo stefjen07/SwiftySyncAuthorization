dac38d9eaf42405268999bdd0bbc40b61291e7ae
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-25 22:29:59 +0300

    Deleting the second Usage file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include "Authorization.h"
-#include "FacebookAuthorization.h"
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #include "GoogleAuthorization.h"
+#else
+#include "Authorization.h"
+#endif
+#include "FacebookAuthorization.h"
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include "Authorization.h"
+#else
+#include "GoogleAuthorization.h"
+#endif
 
 int main() {
     auto googleProvider = GoogleProvider();
